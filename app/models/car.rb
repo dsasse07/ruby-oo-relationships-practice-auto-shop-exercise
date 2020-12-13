febca8989db02aceb7c5bc2d883e8309fe2beba1
Daniel Sasse <dsasse07@gmail.com>
2020-12-13 16:47:56 -0500

    commit tree f2ce40aaa457e099a066da4c81a1377fd96aed88

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,26 @@
 class Car
+  @@all = []
+  attr_reader :make, :model, :owner, :mechanic, :classification
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, owner, mechanic)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner
+    @mechanic = mechanic
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    self.all.map(&:classification).uniq
+  end 
+
+  def self.find_mechanics_by_classification(classification)
+    Mechanic.all.select {|mechanic| mechanic.specialty == classification}
   end
 
 end
--- app/models/mechanic.rb
+++ app/models/mechanic.rb
@@ -1,11 +1,27 @@
 class Mechanic
-
+  @@all = []
   attr_reader :name, :specialty
 
-def initialize(name, specialty)
-  @name = name
-  @specialty = specialty
-end
+  def initialize(name, specialty)
+    @name = name
+    @specialty = specialty
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def cars
+    Car.all.select {|car| car.mechanic == self}
+  end
+
+  def car_owners
+    self.cars.map(&:owner)
+  end
+
+  def car_owners_names
+    self.car_owners.map(&:name)
+  end
 
 end
