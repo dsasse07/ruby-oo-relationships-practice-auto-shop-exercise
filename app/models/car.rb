d7582cbe95c71c9f9ef7dca686d4d59f424876e5
Daniel Sasse <dsasse07@gmail.com>
2020-12-15 17:16:11 -0500

    car.rb parameters renamed for clarity

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,11 +1,26 @@
 class Car
+  @@all = []
+  attr_reader :make, :model, :owner, :mechanic, :classification
 
-  attr_reader :make, :model
-
-  def initialize(make, model, classification)
+  def initialize(make, model, classification, owner_inst, mechanic_inst)
     @make = make
     @model = model
     @classification = classification
+    @owner = owner_inst
+    @mechanic = mechanic_inst
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.classifications
+    self.all.map(&:classification).uniq
+  end 
+
+  def self.find_mechanics_by_classification(classification)
+    Mechanic.all.select {|mechanic| mechanic.specialty == classification}
   end
 
 end
--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,27 @@
 class CarOwner
-
+  @@all = []
   attr_reader :name
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def cars
+    Car.all.select {|car| car.owner == self}
+  end
+
+  def mechanics
+    self.cars.map(&:mechanic)
+  end
+
+  def self.average_number_of_cars
+    (self.all.sum{|owner| owner.cars.count} / self.all.count).to_f
+  end
+    
+  
 end
